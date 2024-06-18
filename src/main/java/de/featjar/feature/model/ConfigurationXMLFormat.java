068e6e9925b685c7b7814dab134a902edb3cbcec
Neha Singh <nehas@mail.uni-paderborn.de>
2024-06-18 23:38:15 +0200

    Extended existing class for xml features. Modifed function accordingly

--- src/main/java/de/featjar/feature/model/ConfigurationXMLFormat.java
+++ src/main/java/de/featjar/feature/model/ConfigurationXMLFormat.java
@@ -14,66 +14,53 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-import de.featjar.base.data.Result;
-import de.featjar.feature.model.mixins.IHasCommonAttributes;
-
-
 public class ConfigurationXMLFormat {
     private Map<String, Object> features;
-    private Map<String, SelectionType> manualSelections;
-    private Map<String, SelectionType> automaticSelections;
+    private Map<String, SelectableFeature> selectableFeatures;
 
     public ConfigurationXMLFormat() {
         this.features = new HashMap<>();
-        this.manualSelections = new HashMap<>();
-        this.automaticSelections = new HashMap<>();
+        this.selectableFeatures = new HashMap<>();
     }
 
-    public void addFeature(IHasCommonAttributes feature, Object value, SelectionType manual, SelectionType automatic) {
-    String name = feature.getName().orElseThrow("Warning null value occured"); // Null validation
-    this.features.put(name, value);
-    this.manualSelections.put(name, manual);
-    this.automaticSelections.put(name, automatic);
+    public void addFeature(String name, Object value, SelectableFeature feature) {
+        this.features.put(name, value);
+        this.selectableFeatures.put(name, feature);
     }
 
     public Map<String, Object> getFeatures() {
         return this.features;
     }
 
-    public Map<String, SelectionType> getManualSelections() {
-        return this.manualSelections;
-    }
-
-    public Map<String, SelectionType> getAutomaticSelections() {
-        return this.automaticSelections;
+    public Map<String, SelectableFeature> getSelectableFeatures() {
+        return this.selectableFeatures;
     }
 
     public void loadFromXML(String filePath) {
         try {
             File file = new File(filePath);
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(file);
-
-            doc.getDocumentElement().normalize();
+            Document doc = parseDocument(file);
 
             NodeList featureNodes = doc.getElementsByTagName("feature");
 
             for (int i = 0; i < featureNodes.getLength(); i++) {
-
                 Element featureElement = (Element) featureNodes.item(i);
-                String name = featureElement.getElementsByTagName("name").item(0).getTextContent();
-                String value = featureElement.getElementsByTagName("value").item(0).getTextContent();
+                String name = featureElement.getAttribute("name");
                 String type = featureElement.getElementsByTagName("type").item(0).getTextContent();
-                SelectionType manual = SelectionType.valueOf(featureElement.getAttribute("manual").toUpperCase());
-                SelectionType automatic = SelectionType.valueOf(featureElement.getAttribute("automatic").toUpperCase());
+                String value = featureElement.getElementsByTagName("value").item(0).getTextContent();
+                SelectionType manual = featureElement.hasAttribute("manual") ?
+                        SelectionType.valueOf(featureElement.getAttribute("manual").toUpperCase()) :
+                        SelectionType.UNDEFINED;
+                SelectionType automatic = featureElement.hasAttribute("automatic") ?
+                        SelectionType.valueOf(featureElement.getAttribute("automatic").toUpperCase()) :
+                        SelectionType.UNDEFINED;
 
                 Object typedValue;
                 switch (type) {
                     case "Double":
                         typedValue = Double.parseDouble(value);
                         break;
-                    case "Float": // can remove it
+                    case "Float":
                         typedValue = Float.parseFloat(value);
                         break;
                     case "Integer":
@@ -88,9 +75,11 @@ public class ConfigurationXMLFormat {
                         break;
                 }
 
+                SelectableFeature selectableFeature = new SelectableFeature(name);
+                selectableFeature.setManual(manual);
+                selectableFeature.setAutomatic(automatic);
                 this.features.put(name, typedValue);
-                this.manualSelections.put(name, manual);
-                this.automaticSelections.put(name, automatic);
+                this.selectableFeatures.put(name, selectableFeature);
             }
 
         } catch (Exception e) {
@@ -100,61 +89,89 @@ public class ConfigurationXMLFormat {
 
     public void saveToXML(String filePath) {
         try {
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.newDocument();
+            Document doc = createDocument();
 
             // Root element
-            Element rootElement = doc.createElement("ConfigurationXMLFormat");
+            Element rootElement = doc.createElement("configuration");
             doc.appendChild(rootElement);
 
             // Features
             for (String name : this.features.keySet()) {
                 Element featureElement = doc.createElement("feature");
 
-                Element nameElement = doc.createElement("name");
-                nameElement.appendChild(doc.createTextNode(name));
-                featureElement.appendChild(nameElement);
-
-                Element valueElement = doc.createElement("value");
-                valueElement.appendChild(doc.createTextNode(this.features.get(name).toString()));
-                featureElement.appendChild(valueElement);
+                featureElement.setAttribute("name", name);
+                featureElement.setAttribute("manual", this.selectableFeatures.get(name).getManual() != null ?
+                        this.selectableFeatures.get(name).getManual().name().toLowerCase() : "undefined");
+                featureElement.setAttribute("automatic", this.selectableFeatures.get(name).getAutomatic() != null ?
+                        this.selectableFeatures.get(name).getAutomatic().name().toLowerCase() : "undefined");
 
                 Element typeElement = doc.createElement("type");
                 typeElement.appendChild(doc.createTextNode(this.features.get(name).getClass().getSimpleName()));
                 featureElement.appendChild(typeElement);
 
-                featureElement.setAttribute("manual", this.manualSelections.get(name).name().toLowerCase());
-                featureElement.setAttribute("automatic", this.automaticSelections.get(name).name().toLowerCase());
+                Element valueElement = doc.createElement("value");
+                valueElement.appendChild(doc.createTextNode(this.features.get(name).toString()));
+                featureElement.appendChild(valueElement);
 
                 rootElement.appendChild(featureElement);
             }
 
-            // Write the content into XML file
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File(filePath));
-
-            transformer.transform(source, result);
+            writeDocument(doc, filePath);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    // Override parseDocument method
+    protected Document parseDocument(File file) throws Exception {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        return dBuilder.parse(file);
+    }
+
+    // Override createDocument method
+    protected Document createDocument() throws Exception {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        return dBuilder.newDocument();
+    }
+
+    // Override writeDocument method
+    protected void writeDocument(Document doc, String filePath) throws Exception {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File(filePath));
+        transformer.transform(source, result);
+    }
+
     public static void main(String[] args) {
         ConfigurationXMLFormat config = new ConfigurationXMLFormat();
-        config.addFeature("Feature1", "Value1", SelectionType.MANUAL, SelectionType.AUTOMATIC);
-        config.addFeature("Feature2", 42.0, SelectionType.MANUAL, SelectionType.AUTOMATIC);
-        config.addFeature("Feature3", true, SelectionType.MANUAL, SelectionType.AUTOMATIC);
-        config.saveToXML("config.xml");
+
+        SelectableFeature feature1 = new SelectableFeature("Feature1");
+        feature1.setManual(SelectionType.MANUAL);
+        feature1.setAutomatic(SelectionType.AUTOMATIC);
+        config.addFeature("Feature1", "StringValue", feature1);
+
+        SelectableFeature feature2 = new SelectableFeature("Feature2");
+        feature2.setManual(SelectionType.MANUAL);
+        feature2.setAutomatic(SelectionType.AUTOMATIC);
+        config.addFeature("Feature2", 43, feature2);
+
+        SelectableFeature feature3 = new SelectableFeature("Feature3");
+        feature3.setManual(SelectionType.MANUAL);
+        feature3.setAutomatic(SelectionType.AUTOMATIC);
+        config.addFeature("Feature3", 123.45, feature3);
+
+        config.saveToXML("Featuremodelconfig1.xml");
 
         ConfigurationXMLFormat loadedConfig = new ConfigurationXMLFormat();
-        loadedConfig.loadFromXML("config.xml");
+        loadedConfig.loadFromXML("Featuremodelconfig1.xml");
         System.out.println(loadedConfig.getFeatures());
-        System.out.println(loadedConfig.getManualSelections());
-        System.out.println(loadedConfig.getAutomaticSelections());
+        System.out.println(loadedConfig.getSelectableFeatures());
     }
 }
+
+
