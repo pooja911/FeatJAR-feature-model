1f50086c61304aacf3b6bd42b8b9d661614e53ff
Neha Singh <nehas@mail.uni-paderborn.de>
2024-07-02 20:34:31 +0200

    Refactor: This will contain enum for automatic and manual states

--- src/main/java/de/featjar/feature/model/SelectionType.java
+++ src/main/java/de/featjar/feature/model/SelectionType.java
@@ -1,4 +1,8 @@
 package de.featjar.feature.model;
+/**
+ * Enumeration representing the selection type of a feature in the feature model.
+ * A feature can be selected either manually by the user or automatically by the system.
+ */
 
 public enum SelectionType {
     MANUAL,
