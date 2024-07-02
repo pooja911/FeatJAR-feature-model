b780d8192227c8ac9dcfd9ab806249e26e3a2be2
Pooja Garg <poojag@mail.uni-paderborn.de>
2024-07-03 00:51:45 +0200

    Refactoring

--- src/main/java/de/featjar/feature/model/Selection.java
+++ src/main/java/de/featjar/feature/model/Selection.java
@@ -1,14 +1,14 @@
 package de.featjar.feature.model;
 
-/** The {@code Selection} enum represents the possible states of a feature within the feature model.
+/** The  enum represents the possible states of a feature within the feature model.
  * It is used to denote whether a feature is:
  * 
- *   {@link #SELECTED} - The feature is selected or enabled.
- *   {@link #UNSELECTED} - The feature is deselected or disabled.
- *   {@link #UNDEFINED} - The feature has no defined selection state (neither selected nor unselected).
+ *   {#SELECTED} - The feature is selected or enabled.
+ *   {#UNSELECTED} - The feature is deselected or disabled.
+ *   {#UNDEFINED} - The feature has no defined selection state (neither selected nor unselected).
  *   
  * This enum is used across various components in the feature management system to track and manage
- * the selection state of features. The {@code toString} method is overridden to provide a string representation
+ * the selection state of features. The  method is overridden to provide a string representation
  * of the enum constants.
  * 
  * 
